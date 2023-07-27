3c11d96b134d5a308a9b903044853f9b29d8cabd
miniface <m.moshek@gmail.com>
2023-07-27 11:19:52 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,77 @@
+"""Python file to serve as the frontend"""
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.agents import AgentExecutor, LLMSingleActionAgent
+from langchain.callbacks import StreamlitCallbackHandler
+
+from document_links import get_info_files
+from tool_utils.retrieval_tools import get_retrieval_qa_tools
+from output_parsers_utils.custom_output_parsers import EnglishOutputParser, FrenchOutputParser
+from prompts_utils.custom_prompts import CustomPromptTemplate, english_agent_template, french_agent_template
+
+
+def load_agent(language="fr"):
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k-0613", streaming=True)
+
+    files = get_info_files(language=language)
+    tools = get_retrieval_qa_tools(files=files, language=language)
+    tool_names = [tool.name for tool in tools]
+
+    if language == "en":
+        prompt_template = english_agent_template
+        custom_parser = EnglishOutputParser()
+
+    elif language == "fr":
+        prompt_template = french_agent_template
+        custom_parser = FrenchOutputParser()
+    else:
+        raise ValueError("Language must be either 'en' or 'fr'")
+
+    custom_prompt = CustomPromptTemplate(
+        template=prompt_template,
+        tools=tools,
+        input_variables=["input", "intermediate_steps"]
+    )
+
+    llm_chain = LLMChain(llm=llm, prompt=custom_prompt)
+
+    custom_agent = LLMSingleActionAgent(
+        llm_chain=llm_chain,
+        output_parser=custom_parser,
+        stop=["\nObservation:"],
+        allowed_tools=tool_names
+    )
+
+    agent = AgentExecutor.from_agent_and_tools(agent=custom_agent, tools=tools, verbose=True)
+
+    return agent
+
+
+agent = load_agent('fr')
+
+# From here down is all the StreamLit UI.
+st.set_page_config(page_title="UBS vs BCV ", page_icon=":robot:")
+st.header("Comparateur de documents bancaires")
+
+
+st.title("UBS vs BCV")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if user_input := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    st.chat_message("user").write(user_input)
+    with st.chat_message("assistant"):
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(input=user_input, callbacks=[st_callback])
+        st.write(response)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
---
+++ document_links.py
@@ -0,0 +1,46 @@
+# Description: This file contains the links to the documents that will be used for the project
+
+def get_info_files(language='en'):
+    if language == "en":
+        bcv_path = 'bank_docs/bcv_general-terms-and-conditions-en.txt'
+        ubs_path = 'bank_docs/ubs_general-terms-and-conditions-en.txt'
+        bcv_name = "bcv_general-terms-and-conditions-en"
+        ubs_name = "ubs_general-terms-and-conditions-en"
+        input_format = "The specific piece of information you want from the document"
+
+    elif language == "fr":
+        bcv_path = 'bank_docs/bcv_general-terms-and-conditions-fr.txt'
+        ubs_path = 'bank_docs/ubs_general-terms-and-conditions-fr.txt'
+        bcv_name = "bcv_general-terms-and-conditions-fr"
+        ubs_name = "ubs_general-terms-and-conditions-fr"
+        input_format = "La pièce spécifique d'information que vous souhaitez obtenir du document"
+
+    else:
+        raise ValueError("Language must be either 'en' or 'fr'")
+
+    store_directory = 'bank_docs/'
+
+    files = [
+        {
+            "name": bcv_name,
+            "input_format": input_format,
+            "path": bcv_path,
+            "store_directory": store_directory + "bcv/" + language + "/",
+            "chunk_size": 1500,
+            "chunk_overlap": 0,
+            "return_source_documents": False,
+            "return_direct": False,
+        },
+        {
+            "name": ubs_name,
+            "input_format": input_format,
+            "path": ubs_path,
+            "store_directory": store_directory + "ubs/" + language + "/",
+            "chunk_size": 1500,
+            "chunk_overlap": 0,
+            "return_source_documents": False,
+            "return_direct": False,
+        },
+    ]
+
+    return files
